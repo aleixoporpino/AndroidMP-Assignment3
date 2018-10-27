0a9f39fe4c6192a083c2adfb08d67f0dd2b8c67b
OguzBayral <oguzbayral93@gmail.com>
2018-10-27 12:24:57 -0400

    Text Line change

--- JoseFilho_OguzBayral_MAPD711_OnlinePurchase/app/src/main/java/centennialcollege/ca/josefilho_oguzbayral_mapd711_onlinepurchase/MainActivity.java
+++ JoseFilho_OguzBayral_MAPD711_OnlinePurchase/app/src/main/java/centennialcollege/ca/josefilho_oguzbayral_mapd711_onlinepurchase/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //hey
+
         /*CustomerDAO customerDAO = new CustomerDAO(this);
 
         Customer customer = customerDAO.login("admin","1223456");
