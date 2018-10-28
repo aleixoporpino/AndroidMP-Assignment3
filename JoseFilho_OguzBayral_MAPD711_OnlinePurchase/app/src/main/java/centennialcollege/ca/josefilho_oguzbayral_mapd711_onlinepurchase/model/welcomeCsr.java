51d11e7050a3c2206c0e58301be51c290e96f624
OguzBayral <oguzbayral93@gmail.com>
2018-10-28 14:55:28 -0400

    SharedPreferences added,customer order activity added

---
+++ JoseFilho_OguzBayral_MAPD711_OnlinePurchase/app/src/main/java/centennialcollege/ca/josefilho_oguzbayral_mapd711_onlinepurchase/model/welcomeCsr.java
@@ -0,0 +1,13 @@
+package centennialcollege.ca.josefilho_oguzbayral_mapd711_onlinepurchase.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcomeCsr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_csr);
+    }
+}
